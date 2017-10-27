993954a9b5b8a8bfef8fb71384750d1877f7065e
BoHuang <bohuang29@hotmail.com>
2017-10-27 15:30:43 +0800

    Initial Commit

---
+++ mwod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mwod
+//
+//  Created by BoHuang on 10/27/17.
+//  Copyright © 2017 BoHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mwod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mwod
+//
+//  Created by BoHuang on 10/27/17.
+//  Copyright © 2017 BoHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
